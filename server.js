e2b38c45e0ee92a09245e90b1c28412de21852ad
rajitha123k <rkanthet@cisco.com>
2025-01-13 14:38:41 -0500

    socket io update

--- server.js
+++ server.js
@@ -1,19 +1,73 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+const { createServer } = require("node:http");
+
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 const app = express();
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http);
+const server = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "https://socketeer.glitch.me/",
+    methods: ["GET", "POST"],
+  },
+});
 const port = process.env.PORT || 3000;
+// const options = {
+//   key: fs.readFileSync("10.16.44.227.key"),
+//   cert: fs.readFileSync("10.16.44.227.pem"),
+//   passphrase: "qwertyuiop@123",
+// };
 
 app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.json());
+
+app.use(cors());
+
+// app.get("/hangup", (req, res) => {
+//   console.log("hangup");
+// });
 
 app.post("/task-routing", (req, res) => {
+  console.log(req.body);
+  const { access_token, destination } = req.body;
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/xml");
   myHeaders.append("Cookie", "JSESSIONID=7430280F23446A3C75D2533EEA6AFF2B");
 
-  const raw =
-    "<Task>\n   <name>NewTaskName</name>\n   <title>NewTaskTitle</title>\n   <description>NewTaskdescription</description>\n   <scriptSelector>CumulusTask</scriptSelector>\n   <requeueOnRecovery>true</requeueOnRecovery>\n   <!-- Indicates if the contact will be re-queued/discarded on \n   SM failure recovery-->\n   <tags>\n      <tag>tag1</tag>\n      <tag>tag2</tag>\n   </tags>\n   <variables>\n    <variable> \n      <name>cv_1</name> \n      <value>access_token</value> \n    </variable>\n        <variable> \n      <name>cv_2</name> \n      <value>destination</value> \n    </variable>\n   </variables>\n</Task>";
+  const raw = `<Task>
+   <name>NewTaskName</name>
+   <title>NewTaskTitle</title>
+   <description>NewTaskdescription</description>
+   <scriptSelector>CumulusTask</scriptSelector>
+   <requeueOnRecovery>true</requeueOnRecovery>
+   <!-- Indicates if the contact will be re-queued/discarded on 
+   SM failure recovery-->
+   <tags>
+      <tag>tag1</tag>
+      <tag>tag2</tag>
+   </tags>
+   <variables>
+    <variable> 
+      <name>cv_1</name> 
+      <value>12345</value> 
+    </variable>
+    <variable> 
+      <name>user_user_videoToken</name> 
+      <value>${access_token}</value> 
+    </variable>
+    <variable> 
+      <name>user_user_videoDestination</name> 
+      <value>${destination}</value> 
+    </variable>
+   </variables>
+</Task>`;
 
   const requestOptions = {
     method: "POST",
@@ -30,26 +84,20 @@ app.post("/task-routing", (req, res) => {
     })
     .catch((error) => console.error(error));
 });
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// https.createServer(options, app).listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+io.on("connection", (socket) => {
+  console.log(`user: ${JSON.stringify(socket.id)}`);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("app_msg", (msg) => {
+    socket.broadcast.emit("app_msg", msg); //Send to all except author/sender
+    //io.emit("app_msg", msg); //Send to all, including author/sender
+  });
 });
 
-// const express = require("express");
-// const webpack = require("webpack");
-// const webpackDevMiddleware = require("webpack-dev-middleware");
-
-// const app = express();
-// const config = require("./webpack.config.js");
-// const compiler = webpack(config);
-
-// app.use(
-//   webpackDevMiddleware(compiler, {
-//     publicPath: config.output.publicPath,
-//   })
-// );
-
-// // Serve the files on port 3000.
-// app.listen(3000, function () {
-//   console.log("Example app listening on port 3000!\n");
-// });
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import "./style.css";
 const { enableDrag } = require("./selfview");
+var socket = io();
+console.log("socketio", socket);
 
-function getAccessTokenFromURL() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get("access_token");
-}
-function getDestinationFromURL() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get("destination");
-}
+const urlParams = new URLSearchParams(window.location.search);
 
-const myAccessToken = getAccessTokenFromURL();
-const destination = getDestinationFromURL();
-
+const myAccessToken = urlParams.get("access_token");
+const destination = urlParams.get("destination");
+const site = urlParams.get("site");
 if (!myAccessToken) {
   alert("Access token is missing. Please provide it in the URL.");
   throw new Error("Access token is missing. Please provide it in the URL.");
@@ -27,6 +22,15 @@ const webex = Webex.init({
 });
 
 webex.config.logger.level = "debug";
+// socket.emit("app_msg", `workflow button pushed`);
+
+socket.on("app_msg", function (msg) {
+  if (site == "client") {
+    alert(msg);
+  }
+
+  console.log("Socket IO Message: " + msg);
+});
 
 webex.meetings
   .register()
@@ -37,28 +41,6 @@ webex.meetings
       .create(destination)
       .then(async (meeting) => {
         console.log("Meeting successfully created");
-        // let data =
-        //   "<Task>\r\n   <name>NewTaskName</name>\r\n   <title>NewTaskTitle</title>\r\n   <description>NewTaskdescription</description>\r\n   <scriptSelector>CumulusTask</scriptSelector>\r\n   <requeueOnRecovery>true</requeueOnRecovery>\r\n   <!-- Indicates if the contact will be re-queued/discarded on \r\n   SM failure recovery-->\r\n   <tags>\r\n      <tag>tag1</tag>\r\n      <tag>tag2</tag>\r\n   </tags>\r\n   <variables>\r\n    <variable> \r\n      <name>cv_1</name> \r\n      <value>access_token</value> \r\n    </variable>\r\n        <variable> \r\n      <name>cv_2</name> \r\n      <value>destination</value> \r\n    </variable>\r\n   </variables>\r\n</Task>";
-
-        // let config = {
-        //   method: "post",
-        //   maxBodyLength: Infinity,
-        //   url: "https://sm1.dcloud.cisco.com/ccp/task/feed/100080",
-        //   headers: {
-        //     "Content-Type": "application/xml",
-        //     Cookie: "JSESSIONID=686F769071BB0A13F344F78F9A0E0447",
-        //   },
-        //   data: data,
-        // };
-
-        // axios
-        //   .request(config)
-        //   .then((response) => {
-        //     console.log(JSON.stringify(response.data));
-        //   })
-        //   .catch((error) => {
-        //     console.log(error);
-        //   });
 
         // Call our helper function for binding events to meetings
         await bindMeetingEvents(meeting);
@@ -85,14 +67,32 @@ async function bindButtonEvents(meeting) {
   const showSelfView = document.getElementById("show-self-view");
   const self = document.getElementById("self");
   const remoteView = document.getElementById("remote-view");
+  const clientConnect = document.getElementById("client-connect");
+  const agentConnect = document.getElementById("agent-connect");
   const dropdown = document.getElementsByClassName("dropdown");
 
   document.getElementById("hangup").addEventListener("click", async () => {
     window.location.href = "/hangup";
-    // await meeting.endMeetingForAll();
+    await meeting.endMeetingForAll();
     await meeting.getMembers().then((members) => {
       console.log("members", members);
     });
+  });
+  console.log("site", site);
+  if (site == "client") {
+    clientConnect.style.display = "none";
+    agentConnect.style.display = "";
+  } else if (site == "agent") {
+    clientConnect.style.display = "";
+    agentConnect.style.display = "none";
+  }
+  clientConnect.addEventListener("click", () => {
+    console.error("workflow button pushed");
+    socket.emit("app_msg", `Agent workflow button pushed`);
+  });
+  agentConnect.addEventListener("click", () => {
+    console.error("workflow button pushed");
+    socket.emit("app_msg", `Client workflow button pushed`);
   });
   videoMuteOff.addEventListener("click", () => {
     console.log("videmute off clicked");
@@ -143,39 +143,7 @@ async function bindMeetingEvents(meeting) {
   const remoteViewAudio = document.getElementById("remote-view-audio");
   const buttonsContainer = document.getElementById("buttons-container");
   const loadingContainer = document.getElementById("loading-container");
-  // meeting.on("error", (err) => {
-  //   console.error(err);
-  // });
 
-  // // Handle media streams changes to ready state
-  // meeting.on("media:ready", (media) => {
-  //   bindButtonEvents(meeting);
-  //   if (!media) {
-  //     return;
-  //   }
-  //   if (media.type === "remoteVideo") {
-  //     document.getElementById("remote-view-video").srcObject = media.stream;
-  //   }
-  //   if (media.type === "remoteAudio") {
-  //     document.getElementById("remote-view-audio").srcObject = media.stream;
-  //   }
-  // });
-
-  // // Handle media streams stopping
-  // meeting.on("media:stopped", (media) => {
-  //   // Remove media streams
-  //   if (media.type === "remoteVideo") {
-  //     document.getElementById("remote-view-video").srcObject = null;
-  //   }
-  //   if (media.type === "remoteAudio") {
-  //     document.getElementById("remote-view-audio").srcObject = null;
-  //   }
-  // });
-
-  // // Of course, we'd also like to be able to leave the meeting:
-  // document.getElementById("hangup").addEventListener("click", () => {
-  //   meeting.leave();
-  // });
   meeting.on("error", (error) => console.log(error, "Meeting Error"));
 
   meeting.on("media:ready", (media) => {
@@ -217,31 +185,6 @@ async function bindMeetingEvents(meeting) {
 // Join the meeting and add media
 // Join the meeting and add media through joinWithMedia method.
 async function joinMeeting(meeting) {
-  // const microphoneStream =
-  //   await webex.meetings.mediaHelpers.createMicrophoneStream({
-  //     echoCancellation: true,
-  //     noiseSuppression: true,
-  //   });
-
-  // const cameraStream = await webex.meetings.mediaHelpers.createCameraStream({
-  //   width: 640,
-  //   height: 480,
-  // });
-  // document.getElementById("self-view").srcObject = cameraStream.outputStream;
-
-  // const meetingOptions = {
-  //   mediaOptions: {
-  //     allowMediaInLobby: true,
-  //     shareAudioEnabled: false,
-  //     shareVideoEnabled: false,
-  //     localStreams: {
-  //       camera: cameraStream,
-  //       microphone: microphoneStream,
-  //     },
-  //   },
-  // };
-
-  // await meeting.joinWithMedia(meetingOptions);
   try {
     const { sendAudio, sendVideo } = await meeting.getSupportedDevices({
       sendAudio: true,
